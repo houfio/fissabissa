b1156aaa4cfc79275f8c1a29bec3b94a87681a6d
Jeremy <jeremyalberts@outlook.com>
2020-01-07 12:38:26 +0100

    initial 3

---
+++ FissaBissa/FissaBissa/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FissaBissa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
